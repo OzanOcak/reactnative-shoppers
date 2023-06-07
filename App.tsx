d65d7a4b6e6876b1d525fcdc4fad1cbefbfd9271
OzanOcak <ozanocak@gmail.com>
2023-06-07 11:20:18 +0300

    initalization

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          doloremque at ullam est libero saepe expedita debitis sunt doloribus
+          vitae.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
